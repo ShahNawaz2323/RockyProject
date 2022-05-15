119ccf3982cce8e4cc8a5d6acc4f6a8f91fb4785
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-05-15 13:41:57 +0500

    Add logic of edit and view of view, Add delete and update category form buttons.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -40,5 +40,20 @@ namespace Rocky.Controllers
             }
             return View(obj);
         }
+
+        //Get - Edit
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
