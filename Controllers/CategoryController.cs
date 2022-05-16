6689b9857c864f56e35d0788bdfb7f04a16d9e0a
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-05-16 12:57:00 +0500

    Delete Data in the DB or crup operation to delete a post.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -69,5 +69,35 @@ namespace Rocky.Controllers
             }
             return View(obj);
         }
+
+        //Get - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //post -Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                _db.Category.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+        }
     }
 }
